0f1a4675f0803acd4258a85e03be29f8bb446bed
Esteban Ruiz (Megasorfer20) <edrp.campus15@gmail.com>
2023-10-12 00:36:31 -0500

    Nav coregido

--- Front/filtro/src/components/Nav.jsx
+++ Front/filtro/src/components/Nav.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../css/Nav.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/Nav.css";
 
 const Nav = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,80 +12,89 @@ const Nav = () => {
 
   const handleLogout = () => {
     setIsLoggedIn(false);
-    window.location.href = '/';
+    window.location.href = "/";
   };
 
   const togglePanel = () => {
     setShowPanel(!showPanel);
   };
-  
+
   return (
-   <div>
-    <nav className="nav">
-      <div className='aling_menu anadir'>
-        <img src="../anadir.png" alt="Logo" />
-        <a href="#">Añadir</a>    
-      </div>
-      <div className='aling_menu refresh'>
-        <img src="../actualizar.png" alt="Logo" />
-        <a href="#">Refrescar</a>
-      </div>
-      <div className='aling_menu delete'>
-        <img src="../Icon material-delete-forever.svg" alt="Logo" />
-        <a href="#">Eliminar</a>
-      </div>
-
-      <div className='aling_menu logo_nav'>
-        <img onClick={togglePanel} src="../KARIO_LOGO.png" alt="Logo" />
-        {showPanel && (
-          <div className="panel">
-            <Link to="/panel"><button className='custom-button'>Opción 1</button></Link>
-            <Link to="/panel"><button className='custom-button'>Opción 2</button></Link>
-            <Link to="/panel"><button className='custom-button'>Opción 3</button></Link>
+    <div>
+      <nav className="nav">
+        <div className="reorganization">
+          <div className="aling_menu anadir">
+            <img src="../anadir.png" alt="Logo" />
+            <a href="#">Añadir</a>
           </div>
-        )}
-      </div>
-       
-      <div className='aling_menu reportar'>
-        <img src="../Icon material-bug-report.svg" alt="Logo" />
-        <Link to="/Report">Reportar</Link>
-      </div>
+          <div className="aling_menu refresh">
+            <img src="../actualizar.png" alt="Logo" />
+            <a href="#">Refrescar</a>
+          </div>
+          <div className="aling_menu delete">
+            <img src="../Icon material-delete-forever.svg" alt="Logo" />
+            <a href="#">Eliminar</a>
+          </div>
+        </div>
 
-      <div className='aling_menu ayuda'>
-        <img src="../boton-web-de-ayuda.png" alt="Logo" />
-        <Link to="/Help">Ayuda</Link>
-      </div>
+        <div className="aling_menu logo_nav reorganization">
+          <img onClick={togglePanel} src="../KARIO_LOGO.png" alt="Logo" />
+          {showPanel && (
+            <div className="panel">
+              <Link to="/panel">
+                <button className="custom-button">Opción 1</button>
+              </Link>
+              <Link to="/panel">
+                <button className="custom-button">Opción 2</button>
+              </Link>
+              <Link to="/panel">
+                <button className="custom-button">Opción 3</button>
+              </Link>
+            </div>
+          )}
+        </div>
 
-      <div className='aling_menu configuracion'>
-        <img className='config' src="../configuraciones.png" alt="Logo" />
-      </div>
-      
-      <div className='aling_menu notificacion'>
-        <img className='noti' src="../activar-el-boton-de-notificaciones.png" alt="Logo" />
-      </div>
+        <div className="reorganization">
+          <div className="aling_menu reportar">
+            <img src="../Icon material-bug-report.svg" alt="Logo" />
+            <Link to="/Report">Reportar</Link>
+          </div>
+          <div className="aling_menu ayuda">
+            <img src="../boton-web-de-ayuda.png" alt="Logo" />
+            <Link to="/Help">Ayuda</Link>
+          </div>
+        </div>
 
-      
-  
-
-      <div className='aling_menu foto_user' >
-             
-        <img onClick={handleUserClick} src="https://avatars.githubusercontent.com/u/47305995?v=4" alt="Usuario" />
-      </div>
-
-    
-        <div className="nav-notifications">
-        <i className="fas fa-bell"></i>
-      </div>
-    
-    </nav>
-    <div className='botonbajo'>
-    {isLoggedIn ? (
+        <div className="reorganization">
+          <div className="aling_menu configuracion">
+            <img className="config" src="../configuraciones.png" alt="Logo" />
+          </div>
+          <div className="aling_menu notificacion">
+            <img
+              className="noti"
+              src="../activar-el-boton-de-notificaciones.png"
+              alt="Logo"
+            />
+          </div>
+          <div className="aling_menu foto_user">
+            <img
+              onClick={handleUserClick}
+              src="https://avatars.githubusercontent.com/u/47305995?v=4"
+              alt="Usuario"
+            />
+          </div>
+        </div>
+        
+      </nav>
+      <div className="botonbajo">
+        {isLoggedIn ? (
           <div className="navr ">
             <button className="custom-button" onClick={handleLogout}>
               Cerrar Sesión
             </button>
           </div>
-        ) : null}</div>
+        ) : null}
+      </div>
     </div>
   );
 };
